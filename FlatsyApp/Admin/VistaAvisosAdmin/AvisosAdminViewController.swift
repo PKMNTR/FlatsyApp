ef0e8f41edd9e448086c3a879cbade43111e6e19
user140663 <user140663@TX005.local>
2018-07-10 22:35:36 -0700

    agrego cambios en el storyboard y agrego arechivos necesarios para los tableview de toda la aplicacion

---
+++ FlatsyApp/Admin/VistaAvisosAdmin/AvisosAdminViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AvisosAdminViewController.swift
+//  FlatsyApp
+//
+//  Created by user140663 on 7/9/18.
+//  Copyright © 2018 pkmntr. All rights reserved.
+//
+
+import UIKit
+
+class AvisosAdminViewController: UIViewController {
+    
+    @IBOutlet weak var avisosTable : UITableView!
+    let data = ["uno","dos"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        avisosTable.dataSource = self
+        avisosTable.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension AvisosAdminViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = avisosTable.dequeueReusableCell(withIdentifier: "AvisoAdminCell") as! AvisosAdminTableViewCell
+        cell.testLabel.text = data[indexPath.row]
+        return cell
+    }
+}
+
+extension AvisosAdminViewController: UITableViewDelegate{
+    
+}
---
+++ FlatsyApp/User/VistaFAQUser/FAQUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FAQUserTableViewCell.swift
+//  FlatsyApp
+//
+//  Created by user140663 on 7/10/18.
+//  Copyright © 2018 pkmntr. All rights reserved.
+//
+
+import UIKit
+
+class FAQUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
